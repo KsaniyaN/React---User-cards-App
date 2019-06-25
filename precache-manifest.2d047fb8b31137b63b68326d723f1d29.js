ba05ed16d35a2878cd39f40b01d72b8133d9808f
KsaniyaN <ksaniya@gmail.com>
2019-06-25 16:10:57 +1000

    Updates

---
+++ precache-manifest.2d047fb8b31137b63b68326d723f1d29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc297e534218e4877b439b1be3a24a09",
+    "url": "/test-01/index.html"
+  },
+  {
+    "revision": "ae4a7957273a3dc90847",
+    "url": "/test-01/static/css/main.40a581ca.chunk.css"
+  },
+  {
+    "revision": "a987bc6dede4cb72e73d",
+    "url": "/test-01/static/js/2.e7e9a4bf.chunk.js"
+  },
+  {
+    "revision": "ae4a7957273a3dc90847",
+    "url": "/test-01/static/js/main.96a932f4.chunk.js"
+  },
+  {
+    "revision": "7216cea763fb1fcc283e",
+    "url": "/test-01/static/js/runtime~main.cc1e0600.js"
+  }
+]);
